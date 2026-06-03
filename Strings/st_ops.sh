652e259e75d399bb8df7d58be62840db6ba3ed35
Mohit <ghsmohit2004@gmail.com>
2026-06-03 19:10:21 +0530

    Create st_ops.sh

---
+++ Strings/st_ops.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+echo "------------------------------"
+name="ByteBandit-100"
+echo "Welcome by $name <> "
+
+read -p "Want to do string operations(yes/no): " op
+if [[ "${op,,}"  == "yes" ]]
+then
+	#taking string user input 
+	read -p "enter first string: " str1
+	read -p "enter second string: " str2
+	
+	#string length
+	read -p "want to know string length(yes/no): " op
+	if [[ "${op,,}" == "yes" ]]; then
+	echo "Length of $str1 is: ${#str1}"
+	echo "Length of $str2 is: ${#str2}"
+	fi
+	
+	#string comparison
+	read -p "want to compare strings(yes/no): " op
+	if [[ "${op,,}" == "yes" ]]; then
+	if [[ "$str1" == "$str2" ]]
+	then
+		echo "\"$str1\" and \"$str2\" are equal. "
+	else
+		echo "\"$str1\" and \"$str2\" are not equal. "
+	fi
+	fi
+	
+	
+	read -p "want to do string lowercase & UPPERCASE(yes/no): " op
+	if [[ "${op,,}" == "yes" ]]; then
+	#lower case                also to use tr [:upper:] [:lower:] vise versa
+	echo "first string lower \"${str1,,}\""
+	echo "second string lower \"${str2,,}\""
+
+	#upper case 
+	echo "FIRST STRING UPPER \"${str1^^}\""
+	echo "SECOND STRING UPPER \"${str2^^}\""
+	fi
+
+fi
+echo "GoodBye :>"
+echo "------------------------------"
